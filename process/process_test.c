72abd2805d03903497b4f41e25c1b746a743a358
xk19 <wangkang957@163.com>
2024-12-16 20:27:36 +0800

    study waitpid

--- process/process_test.c
+++ process/process_test.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main()
 {
@@ -8,23 +10,29 @@ int main()
   
   if(id == 0)
   {
-    while(1)
+    int cnt = 5;
+    while(cnt)
     {
-      printf("子进程, pid：%d, ppid: %d, id: %d\n", getpid(), getppid(), id);
+      printf("子进程, pid：%d, ppid: %d, cnt: %d\n", getpid(), getppid(), cnt--);
       sleep(1);
     }
+  //运行完
+  //1. 代码完，结果对
+  //2. 代码完，结果不对
+  //3. 代码没跑完，出异常了
+  exit(10);//进程退出
   }
-  else if(id > 0)
+
+  //父进程
+  sleep(8);
+  int status = 0;
+  pid_t ret = waitpid(id, &status, 0);
+  if(id > 0)
   {
-    while(1)
-    {
-      printf("父进程, pid：%d, ppid: %d, id: %d\n", getpid(), getppid(), id);
-      sleep(1);
-    }
+    printf("wait success: %d, sig number: %d, child exit code:%d\n",
+        ret, (status & 0x7F), (status >> 8) & 0xFF);
   }
-  else
-  {
-  }
+  sleep(5);
 
   return 0;
 }
